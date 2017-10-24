7c658dbd3b60c7ce03191acbd2988b25a8aa7d54
Andrew Fong <id@andrewfong.com>
2017-10-23 22:53:31 -0700

    A shell script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install nvm
+
+brew install yarn
+
+brew intall rbenv
+
+brew install python3
+
+pip3 install pipenv --user
+
